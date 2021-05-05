8ef7cdf9677b62f79dae890dbbd04c7b72d63172
CaioMorenoGoulart <caiogoulartprogramacao@gmail.com>
2021-05-05 11:59:16 -0300

    feat: Exercicio 4 concluido

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
-$( "button" ).click(function() {
+$( ".botaoCortesia" ).click(function() {
   console.log("Amigo estou aqui")
 });
 $( ".mudaTexto p" ).click(function() {
-  $(".mudaTexto p").text("Meu texto")
+  $(this).text("Meu texto")
 });
-//
+$( ".troca" ).click(function() {
+  $(".boxOffBoxes2 .box2").insertAfter('.boxOffBoxes2 .box3');
+});
+$( ".valor" ).click(function() {
+  var $input = (".input1")
+  $(".inputloc").html("<p>"+$input.attr);
+});
